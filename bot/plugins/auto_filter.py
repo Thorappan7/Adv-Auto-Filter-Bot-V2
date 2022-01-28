f59945e17a10b3f1f46cf3965814f9efbb06ee8e
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-28 17:34:03 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -205,7 +205,7 @@ async def auto_filter(bot, update):
             await update.reply_photo(
                 photo = "https://telegra.ph/file/65ba65b8eab1064c901cc.jpg",
                 chat_id = update.chat.id,                           
-                caption = TEXT.format(message.from_user.mention),
+                caption = TEXT,
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
